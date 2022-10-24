a70df7dca0a0dbc3e5c5e638e38b5d1fdb34571f
ita-jp <tn00gm@gmail.com>
2022-10-24 23:43:42 +0900

    htmx

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/")
@@ -11,5 +14,11 @@ public class IndexController {
     @GetMapping
     public String index() {
         return "index";
+    }
+
+    @GetMapping(value = "swap", headers = "HX-Request")
+    @ResponseBody
+    public String swap() {
+        return "<p id=\"time\">" + LocalDateTime.now() + "</p>";
     }
 }
